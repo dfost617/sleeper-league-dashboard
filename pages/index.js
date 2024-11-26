d8298d3d82885c4f9413ba83545f3ffc16c9ba21
dfost617 <130093523+dfost617@users.noreply.github.com>
2024-11-26 16:28:50 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import LeagueDashboard from '../components/LeagueDashboard';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <LeagueDashboard />
+    </div>
+  );
+}
